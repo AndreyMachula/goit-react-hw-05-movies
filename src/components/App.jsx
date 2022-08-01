b0d53c5f0541dbf1d3fe77a4221d3a28f90f113b
AndreyMachula <amcz2020@gmail.com>
2022-08-01 12:02:26 +0200

    adds component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,45 @@
-export const App = () => {
+import { lazy, Suspense } from "react";
+import { Routes, Route } from "react-router-dom";
+import {ThreeDots} from "react-loader-spinner";
+import Container from "./Container";
+import Navigation from "./Navigation";
+import NotFound from "./NotFound";
+
+const HomePage = lazy(() =>
+  import("./HomePage" /* webpackChunkName: 'home-page' */)
+);
+const MoviesPage = lazy(() =>
+  import("./MoviesPage" /* webpackChunkName: 'movies-page' */)
+);
+const MovieDetailsPage = lazy(() =>
+  import("./MovieDetailsPage" /* webpackChunkName: 'details-page' */)
+);
+
+function App() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Container>
+      <Navigation />
+      <Suspense
+        fallback={
+          <ThreeDots
+            type="ThreeDots"
+            color="#2196f3"
+            height={70}
+            width={70}
+            timeout={2000}
+          />
+        }
+      >
+        <Routes>
+          <Route path="/" element={<HomePage />}></Route>
+          <Route path="/movies" element={<MoviesPage />}></Route>
+          <Route path="/movies/:slug/*" element={<MovieDetailsPage />}></Route>
+
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
+      </Suspense>
+    </Container>
   );
-};
+}
+
+export default App;
